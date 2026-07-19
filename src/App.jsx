4f2794489bd60f23746ec1ce7a67bbe538557021
Sennane Ilyas <sennaneilyas26@gmail.com>
2026-07-19 17:33:09 +0100

    Initial scaffold: Vite + React + Tailwind v4 + folder structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-white h-screen w-screen flex items-center justify-center">
+      <div className="w-36 h-36 bg-black rounded-full flex items-center justify-center">
+        <span className="text-red-500 font-bold text-4xl">Hi</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
